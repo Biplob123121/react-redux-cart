fe709a0b4c99dcceeefcbdbd22e74cab1edb3fd5
Biplob123121 <biplobkb123@gmail.com>
2022-06-05 10:36:00 +0600

    initailize and completed header section

---
+++ src/components/CartsData.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CartsData = () => {
+    return (
+        <div>
+            <h2>This is cart data</h2>
+        </div>
+    );
+};
+
+export default CartsData;
